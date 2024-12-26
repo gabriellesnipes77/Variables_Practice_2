60343499e47b912f4f1aee2a15dc86389d13e3d0
BIG DON <BIG DON@MSI>
2024-12-26 13:37:41 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Variables_Practice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            Console.WriteLine("{0} {1}", double.MinValue, double.MaxValue);
+
+        }
+    }
+}
